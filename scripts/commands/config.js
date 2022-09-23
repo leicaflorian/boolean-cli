6f836b1a38e2e3b7fcd64113f3f36a74cb4d539b
leicaflorian <florian.leica@gmail.com>
2022-09-23 16:40:14 +0200

    Updates main description

--- scripts/commands/config.js
+++ scripts/commands/config.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
 const Conf = require("conf");
+const logs = require("../utilities/logs");
+const chalk = require("chalk");
 
 // Create a Configstore instance.
 const config = new Conf({});
@@ -7,19 +9,20 @@ const config = new Conf({});
 module.exports = function (program) {
   program
     .command("config")
-    .description("Allow to specify global configs.")
+    .description("Allow to specify global configurations.")
     .option("-f, --video-folder <path>", "Path to the Google Drive video folder.")
+    .option("-m, --multipart <bool>", "Boolean that indicate if by default, video files will have more than one part. This will be used to suggest the right part name.")
     .option("-a, --all", "Read all existing config.")
     .option("-r, --reset", "Reset and remove all existing config.")
     .action((options) => {
       if (options.reset) {
         config.clear();
-        return console.log("All settings has been removed");
+        return console.log("All settings has been reset");
       }
 
       if (options.all) {
         for (const conf of config) {
-          console.log(conf);
+          logs.info(`[${chalk.bold(conf[0])}]`, chalk.green(conf[1]));
         }
 
         return;
@@ -31,6 +34,14 @@ module.exports = function (program) {
         }
 
         config.set("videoFolder", options.videoFolder);
+
+        logs.info("[Config]", "Destination folder set as " + chalk.green(config.get("videoFolder")))
+      }
+
+      if (options.multipart) {
+        config.set("multipart", options.multipart.toLowerCase().trim() === "true");
+
+        logs.info("[Config]", "Multipart option set as " + chalk.green(config.get("multipart")))
       }
     });
 
--- scripts/commands/main.js
+++ scripts/commands/main.js
@@ -1,10 +1,14 @@
 const packageJson = require("../../package.json");
+const chalk = require("chalk")
 
 module.exports = function (program, conf) {
   const v = packageJson.version;
 
   program
     .name("boolean-cli")
-    .description("CLI to some utilities useful for Boolean")
+    .description(`CLI with some useful utilities for Boolean collaborators.\n\n` +
+      `The CLI can be configured to read and write to a Google Drive folder.\n`+
+      `To configure the path to this folder, just run 
+      ${chalk.yellow("boolean-cli config -f [folder_path]")}`)
     .version(v, "-v, --version");
 }
